2759e0ca77b0cd791aa7caaf6eac881b7de9f8fc
John Furtado <jf8846@gmail.com>
2018-10-01 17:53:52 -0400

    scrape and log symbols from yahoo finance most active page

--- src/main/java/selenium/LaunchChrome.java
+++ src/main/java/selenium/LaunchChrome.java
@@ -1,17 +1,25 @@
 package selenium;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class LaunchChrome {
 
 	public static void main(String[] args) {
 		
+		// path to chrome webdriver exe
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\John\\careerDevs\\Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.get("http://www.google.com");
-		System.out.println(driver.findElement(By.xpath("//a[@class='gb_P']")).getText());
+		driver.get("https://www.finance.yahoo.com/most-active");
+		List<WebElement> symbols = driver.findElements(By.xpath("//a[@class='Fw(b)']"));
+		
+		for (WebElement symbol : symbols) {
+			System.out.println(symbol.getText());
+		}
 		
 	}
 
